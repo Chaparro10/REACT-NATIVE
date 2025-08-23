db9276272ccc0758ee5b9fca71f3609d0db90484
Kevin Chaparro <kevinchaparro@ElKevin2.local>
2025-08-22 18:01:46 -0700

    metodos en el contexto

--- 01-RESPASO-REACT-TYPESCRIPT/src/components/LoginPage.tsx
+++ 01-RESPASO-REACT-TYPESCRIPT/src/components/LoginPage.tsx
@@ -1,26 +1,32 @@
-
-import React from 'react'
-import { useAuthContext } from '../context/AuthContex'
+import { useAuthContext } from "../context/AuthContex";
 
 export const LoginPage = () => {
-    const {isChecking} = useAuthContext();
+  const { isChecking, isAuthenticated,loginWithEmailPassword,user,logout } = useAuthContext();
 
-
-    if(isChecking){
-        return(
-            <h1>Verificando usuario</h1>
-        )
-    }
-
+  if (isChecking) {
+    return <h1>Verificando usuario</h1>;
+  }
 
   return (
     <div>
-        <h1>
-            LoginPage
-        </h1>
-        <span>
-            {isChecking}
-        </span>
+      <h1>LoginPage</h1>
+      {isAuthenticated ? (
+        <div>
+          <h1>Bienvenido</h1>
+          <pre>{JSON.stringify(user,null,2)}</pre>
+          <button onClick={()=>logout()} className="bg-blue-500 p-2 text-white rounded-xl mt-2">
+            Salir
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3>Ingresar a la aplicacion</h3>
+          <button  onClick={()=>loginWithEmailPassword("kevin@gmail.com","124")}
+          className="bg-blue-500 p-2 text-white rounded-xl mt-2">
+            Ingresar
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
--- 01-RESPASO-REACT-TYPESCRIPT/src/context/AuthContex.tsx
+++ 01-RESPASO-REACT-TYPESCRIPT/src/context/AuthContex.tsx
@@ -1,39 +1,74 @@
-import { createContext, useContext, useEffect, useState, type PropsWithChildren } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type PropsWithChildren,
+} from "react";
 
+enum AuthStatus {
+  "checking",
+  "authenticated",
+  "unauthenticated",
+}
+interface User {
+  name: string;
+  email: string;
+  password: string;
+}
+interface AuthState {
+  status: AuthStatus;
+  token?: string;
+  user?: User;
+  isChecking: boolean;
+  isAuthenticated: boolean;
 
-
-
- enum AuthStatus {
-    'checking',
-    'authenticated',
-    'unauthenticated',
- }
- interface User{
-    email:string,
-    password:string
- }
-interface AuthState{
-    status:AuthStatus,
-    token?:string,
-    user?:User,
-    isChecking:boolean
+  //metodos
+  loginWithEmailPassword: (email: string, password: string) => void;
+  logout:()=>void;
 }
 
-export const AuthContext=createContext({} as AuthState);
-export const useAuthContext=()=>useContext(AuthContext);
+export const AuthContext = createContext({} as AuthState);
+export const useAuthContext = () => useContext(AuthContext);
 
-export const AuthProvider=({children}:PropsWithChildren)=>{
-    const [status,setStatus]=useState<AuthStatus>(AuthStatus.checking);
-    useEffect(()=>{
-            setTimeout(()=>{
-                setStatus(AuthStatus.unauthenticated)
-            },1500);
-    },[])
-    return (
-        <>
-        <AuthContext.Provider value={{status:AuthStatus.checking,isChecking:status ==AuthStatus.checking}}>
+export const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [status, setStatus] = useState<AuthStatus>(AuthStatus.checking);
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setStatus(AuthStatus.unauthenticated);
+    }, 1500);
+  }, []);
+
+  const loginWithEmailPassword = (email: string, password: string) => {
+    setUser({
+      name: "Kevin prueba",
+      email: email,
+      password: password,
+    });
+    setStatus(AuthStatus.authenticated);
+  };
+
+  const logout=()=>{
+    setStatus(AuthStatus.unauthenticated);
+    setUser(undefined);
+  }
+
+  return (
+    <>
+      <AuthContext.Provider
+        value={{
+          status: AuthStatus.checking,
+          user: user,
+          isChecking: status == AuthStatus.checking,
+          isAuthenticated: status == AuthStatus.authenticated,
+          loginWithEmailPassword: loginWithEmailPassword,
+          logout:logout
+        }}
+      >
         {children}
-        </AuthContext.Provider>
-        </>
-    )
-}
+      </AuthContext.Provider>
+    </>
+  );
+};
