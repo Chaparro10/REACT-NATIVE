8da33ed8701e8270ed70988947869b9ef6cf20b2
Kevin Chaparro <kevinchaparro@ElKevin2.local>
2025-08-25 16:24:01 -0700

    fuentes personalizadas y diseño inicial

---
+++ 04-calculator-app/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+ const Index =()=> {
+    return (
+      <View>
+        <Text style={{fontSize:35,color:'white'}}>CALCULATOR APP</Text>
+      </View>
+    )
+}
+
+export default Index
