11c38e07c2f49f703ba5dc93824d7ba82a8cc92b
Kevin Chaparro <kevinchaparro@ElKevin2.local>
2025-09-12 13:11:27 -0700

    custom text + default props

--- 04-calculator-app/app/index.tsx
+++ 04-calculator-app/app/index.tsx
@@ -1,11 +1,16 @@
+import ThemeText from '@/components/ThemeText'
 import { globalStyles } from '@/styles/global-styles'
 import { Text, View } from 'react-native'
 
  const Index =()=> {
     return (
       <View style={globalStyles.calculatorContainer}>
-        <Text style={globalStyles.mainResult}>50x50</Text>
-        <Text style={globalStyles.subResult}>250</Text>
+        <ThemeText
+        variant='h1'
+        >
+          50x5000
+        </ThemeText>
+        <ThemeText variant='h2'>250</ThemeText>
       </View>
     )
 }
